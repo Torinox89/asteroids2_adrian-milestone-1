e4a110a66dd016a20c5960edafbb913d69b20488
Adrian Mitre <mitrehealthcare@gmail.com>
2022-07-21 13:20:59 -0700

    class ship

--- asteroids2.js
+++ asteroids2.js
@@ -30,3 +30,99 @@ function SetupCanvas(){
 
     document.body.addEventListener("keydown", HandleKeyDown);
     document.body.addEventListener("keyup", HandleKeyUp);
+
+    // high scores: retrives-locally stored
+    if (localStorage.getItem(localStorageName) == null) {
+        highScore = 0;
+    } else {
+        highScore = localStorage.getItem(localStorageName);
+    }
+ 
+    Render();
+}
+
+//game  over:event handling
+function HandleKeyDown(e){
+    keys[e.keyCode] = true;
+}
+function HandleKeyUp(e){
+    keys[e.keyCode] = false;
+    if (e.keyCode === 32){
+        bullets.push(new Bullet(ship.angle));
+    }
+}
+ 
+class Ship {
+    constructor() {
+        this.visible = true;
+        this.x = canvasWidth / 2;
+        this.y = canvasHeight / 2;
+        this.movingForward = false;
+        this.speed = 0.1;
+        this.velX = 0;
+        this.velY = 0;
+        this.rotateSpeed = 0.001;
+        this.radius = 15;
+        this.angle = 0;
+        this.strokeColor = 'white';
+        // Used to know where to fire the bullet from
+        this.noseX = canvasWidth / 2 + 15;
+        this.noseY = canvasHeight / 2;
+    }
+    Rotate(dir) {
+        this.angle += this.rotateSpeed * dir;
+    }
+    Update() {
+        // Get current direction ship is facing
+        let radians = this.angle / Math.PI * 180;
+ 
+        // If moving forward calculate changing values of x & y
+        // If you want to find the new point x use the 
+        // formula oldX + cos(radians) * distance
+        // Forumla for y oldY + sin(radians) * distance
+        if (this.movingForward) {
+            this.velX += Math.cos(radians) * this.speed;
+            this.velY += Math.sin(radians) * this.speed;
+        }
+        // If ship goes off board place it on the opposite
+        // side    
+        if (this.x < this.radius) {
+            this.x = canvas.width;
+        }
+        if (this.x > canvas.width) {
+            this.x = this.radius;
+        }
+        if (this.y < this.radius) {
+            this.y = canvas.height;
+        }
+        if (this.y > canvas.height) {
+            this.y = this.radius;
+        }
+        // Slow ship speed when not holding key
+        this.velX *= 0.99;
+        this.velY *= 0.99;
+ 
+        // Change value of x & y while accounting for
+        // air friction    
+        this.x -= this.velX;
+        this.y -= this.velY;
+    }
+    Draw() {
+        ctx.strokeStyle = this.strokeColor;
+        ctx.beginPath();
+        // Angle between vertices of the ship
+        let vertAngle = ((Math.PI * 2) / 3);
+ 
+        let radians = this.angle / Math.PI * 180;
+        // Where to fire bullet from
+        this.noseX = this.x - this.radius * Math.cos(radians);
+        this.noseY = this.y - this.radius * Math.sin(radians);
+ 
+        for (let i = 0; i < 3; i++) {
+            ctx.lineTo(this.x - this.radius * Math.cos(vertAngle * i + radians), this.y - this.radius * Math.sin(vertAngle * i + radians));
+        }
+        ctx.closePath();
+        ctx.stroke();
+    }
+}
+ 
