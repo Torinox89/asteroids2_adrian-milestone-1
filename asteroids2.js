c029b8bd570d9e0a8cfa6ce9f852c2e6184456c1
Adrian Mitre <mitrehealthcare@gmail.com>
2022-07-25 23:16:51 -0700

    More comments are added in the development of the code

--- asteroids2.js
+++ asteroids2.js
@@ -11,6 +11,7 @@ let lives = 3;
 let highScore;
 let localStorageName = "HighScore";
  
+//Loading Setup Canvas
 document.addEventListener('DOMContentLoaded', SetupCanvas);
  
 function SetupCanvas(){
@@ -22,10 +23,11 @@ function SetupCanvas(){
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ship = new Ship();
  
-    for(let i = 0; i < 8; i++){         //for one o more ships
+    //Crate moro than  one ship
+    for(let i = 0; i < 8; i++){         
         asteroids.push(new Asteroid());
     }
-
+//  handling the events like: keydown and keyup
     document.body.addEventListener("keydown", HandleKeyDown);
     document.body.addEventListener("keyup", HandleKeyUp);
 
@@ -39,7 +41,8 @@ function SetupCanvas(){
     Render();
 }
 
-//game  over:event 
+// At the end of the game: deactivate the event handling, 
+// then it is possible to move the event handling functions.
 function HandleKeyDown(e){
     keys[e.keyCode] = true;
 }
@@ -49,7 +52,7 @@ function HandleKeyUp(e){
         bullets.push(new Bullet(ship.angle));
     }
 }
- 
+// Create class Ship
 class Ship {
     constructor() {
         this.visible = true;
@@ -75,11 +78,20 @@ class Ship {
         let radians = this.angle / Math.PI * 180;
  
       
+        // When moving forward: you can calculate
+        // changing values of x & y
+        // Finding the new point X, use this
+        // formula: 
+        //oldX + cos(radians) * distance
+        //oldY + sin(radians) * distance
         if (this.movingForward) {
             this.velX += Math.cos(radians) * this.speed;
             this.velY += Math.sin(radians) * this.speed;
         }
         
+        //When the ship moves out of the frame of reference on Canvas: 
+        // the ship is put on the opposite side.
+
         if (this.x < this.radius) {
             this.x = canvas.width;
         }
@@ -97,21 +109,26 @@ class Ship {
         this.velY *= 0.99;
 
    
-        // air friction    
+        //    Taking into account the friction: 
+        //    the values of x and y are changed.
         this.x -= this.velX;
         this.y -= this.velY;
     }
     Draw() {
         ctx.strokeStyle = this.strokeColor;
         ctx.beginPath();
-        //vertices of the ship:  Angle in between 
+        // Angle between vertices:  of the ship
         let vertAngle = ((Math.PI * 2) / 3);
  
         let radians = this.angle / Math.PI * 180;
-        // fire bullet: origin
+        // fire bullet: origin in the ship (nose)
         this.noseX = this.x - this.radius * Math.cos(radians);
         this.noseY = this.y - this.radius * Math.sin(radians);
- 
+   
+        // allows to connect with a line the points 
+        // (in the 360 degrees of the angles of the ship) 
+        // in the iteration. Taking into account the 
+        //conversion of degrees to radians on the x and y axis.
         for (let i = 0; i < 3; i++) {
             ctx.lineTo(this.x - this.radius * Math.cos(vertAngle * i + radians), this.y - this.radius * Math.sin(vertAngle * i + radians));
         }
@@ -137,6 +154,7 @@ class Bullet{
         this.x -= Math.cos(radians) * this.speed;
         this.y -= Math.sin(radians) * this.speed;
     }
+   // The creation of the bullets with the function fillRect
     Draw(){
         ctx.fillStyle = 'white';
         ctx.fillRect(this.x,this.y,this.width,this.height);
@@ -146,6 +164,8 @@ class Bullet{
 class Asteroid{  
     constructor(x,y,radius,level,collisionRadius) {
         this.visible = true;
+        //  Obtaining an integer in the random calculation 
+        // of the point of origin of a new asteroid.
         this.x = x || Math.floor(Math.random() * canvasWidth);
         this.y = y || Math.floor(Math.random() * canvasHeight);
         this.speed = 0.8;
@@ -177,6 +197,7 @@ class Asteroid{
         ctx.beginPath();
         let vertAngle = ((Math.PI * 3) /8);
         var radians = this.angle / Math.PI * 180;
+        //
         for(let i = 0; i < 6; i++){
             ctx.lineTo(this.x - this.radius * Math.cos(vertAngle * i + radians), this.y - this.radius * Math.sin(vertAngle * i + radians));
         }
